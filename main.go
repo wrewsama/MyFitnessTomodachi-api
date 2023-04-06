72b25ccb265b4fc0942c0652e121145236b02799
wrewsama <andrewlzs512@gmail.com>
2023-04-06 18:00:06 +0800

    test gin route

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import "github.com/gin-gonic/gin" 
 
 func main() {
-	fmt.Print("test")
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"msg": "test",
+		})
+	})
+	r.Run()
 }
